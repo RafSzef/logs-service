2fe4b73f9a76ca587c3f048bbfadb3c45fdd5981
Rafał Szefler <59801632+RafSzef@users.noreply.github.com>
2022-10-15 13:01:56 +0200

    first commit

---
+++ src/main/java/pl/niepracuj/logs_service/controller/LogController.java
@@ -0,0 +1,21 @@
+package pl.niepracuj.logs_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/logs")
+@RequiredArgsConstructor
+public class LogController {
+
+    private final LogMapper logMapper;
+    private final LogService logService;
+
+    @PostMapping
+    public LogDto createLog(@RequestBody LogDto logDto) {
+        return logMapper.toDto(logService.saveLog(logMapper.toNewEntity(logDto)));
+    }
+}
---
+++ src/test/java/pl/niepracuj/logs_service/LogsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.niepracuj.logs_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
